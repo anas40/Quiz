3c139fa4c29d352b305a5a34cb942c075c0cf401
anas40 <anasbeg50@gmail.com>
2020-03-25 22:26:39 +0530

    final score show option

---
+++ src/components/FinalScore.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-jumbotron>
+        <div>Your Final Score was: {{numCorrect}}/{{totalQuestions}}</div>
+    </b-jumbotron>
+    <b-button @click="replay" variant="primary">Replay</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        numCorrect:Number,
+        totalQuestions:Number
+    },
+    methods:{
+        replay(){
+            this.$emit('replay')
+        }
+    }
+};
+</script>
+
+<style scoped>
+.jumbotron{
+    margin: 30px 0;
+}
+.jumbotron>div{
+    font-size:24px;
+    color:black;
+}
+</style>
