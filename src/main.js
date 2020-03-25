581dc93eb5ad62e8b090ef8a7674d996913007d4
anas40 <anasbeg50@gmail.com>
2020-03-25 22:26:22 +0530

    nohing

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-
 import App from './App.vue'
 
 // Install BootstrapVue and icons
